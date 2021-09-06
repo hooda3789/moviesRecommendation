a451aea036029ff633a526ccad86af9a1d23d497
Ankit Kumar <kumarankit3789@gmail.com>
2021-09-06 12:00:54 +0530

    without styling

--- moviesrecommendation/src/App.js
+++ moviesrecommendation/src/App.js
@@ -36,18 +36,47 @@ Horror : [{
 ]
 }
 
-
+var genre = Object.keys(mymovies);
 function App() {
-  const [selectGenre, setGenre] = useState("");
+  var [selectGenre, setGenre] = useState("");
 
   function genreClickHandler(genre) {
+    // var myGenre = mymovies[genre];
+    // var myvalues = Object.keys(myGenre);
+    // selectGenre = {}
+    // for(let i=0; i<myvalues.length; i++){
+    //   console.log(myGenre[i]);
+    //   selectGenre[];
+    // }
+
+    // console.log("selectGernre", selectGenre)
+    // setGenre(selectGenre);
     setGenre(genre);
   }
+
 
   return (
     <div className="App">
       <h1>🎥 Movies Recommendation</h1>
       <h4>Select a genre to get started</h4>
+      {genre.map(function(item){
+          return(
+            <button id="genrebtn" onClick = {()=>genreClickHandler(item)}>{item}</button>
+          )
+      })}
+      <hr />
+      <div>
+        {
+          mymovies[selectGenre].map((movie) =>{
+            return (<ul>
+              <li>
+                <h4>{movie.name}</h4>
+                <h4>{movie.ratings}</h4>
+              </li>
+            </ul>
+             ) }
+          )}
+      </div>
       
     </div>
   );
